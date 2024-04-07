2439c10262e5d0668354e3b5aae8c7f895929e67
BoxDmitry <52531675+BoxDmitry@users.noreply.github.com>
2024-04-07 19:03:47 +0300

    🗑 Удалены неиспользуемые файлы

--- resources/event.object.js
+++ resources/event.object.js
@@ -189,16 +189,8 @@
           return;
         }
 
-        let moderRoad = false;
-
-        if (window.appChrome.user.expertise && window.appChrome.user.expertise.moderatorExpertise) {
-          window.appChrome.user.expertise.moderatorExpertise.forEach((zone) => {
-            if (zone.categoryGroupIds.indexOf("rd_group") !== -1) moderRoad = true;
-          });
-        }
-
         /* Модуль для этой категории отключен, добавляем объект в стоп-лист */
-        if (!settingMaster["object:" + setting.category] || setting.category === "rd_el" && !moderRoad && !window.appChrome.user.yandex) {
+        if (!settingMaster["object:" + setting.category]) {
           stopList.push(ID);
           return;
         }
