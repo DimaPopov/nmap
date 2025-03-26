ec922fe7a277cc379d39e8bc35578c5047386128
RomanSh-shell <151455107+RomanSh-shell@users.noreply.github.com>
2025-03-27 03:47:37 +0500

    Настройки расширения возвращены в логичное место

--- resources/init.js
+++ resources/init.js
@@ -424,22 +424,22 @@
       return;
     }
 
-    /* Ждем клика по полезным ссылкам */
-    $(".nk-help-view").on('click', () => {
+    /* Ждем клика по аватарке */
+    $(".nk-user-bar-view__user-icon").on('click', () => {
       setTimeout(() => {
-        const parent = $("body > div:nth-child(9) > div > div > div > div.nk-menu.nk-menu_theme_islands.nk-menu_size_l > div:nth-child(1)");
+        const parent = $("body > .nk-portal > .nk-popup > .nk-size-observer > .nk-popup__content > .nk-menu > .nk-menu__group:first-of-type");
         const id = chrome.runtime.id;
 
         if(!parent.find('div[data-link="chrome-extension://' + id + '/index.html"]').length){
           parent.append('<div class="nk-menu-item nk-menu-item_theme_islands nk-menu-item_size_l" data-link="chrome-extension://' + id + '/index.html" tabindex="-1">Настройки</div>');
           const button = parent.find('div[data-link="chrome-extension://' + id + '/index.html"]');
-  
+
           button.hover(() => {
             button.addClass("nk-menu-item_hovered");
           }, () => {
             button.removeClass("nk-menu-item_hovered");
           });
-  
+
           button.on("click", () => {
             chrome.runtime.sendMessage({method: "openSetting"});
           });
